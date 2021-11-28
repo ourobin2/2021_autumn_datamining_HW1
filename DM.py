a3ec417615b5a9ec63e454e373c5cedfc53457b4
ourobin2 <39935342+ourobin2@users.noreply.github.com>
2021-11-28 16:15:38 +0800

    final version

--- DM.py
+++ DM.py
@@ -1,8 +1,16 @@
 import csv
+import copy
+import time
+
+
 
 path = 'dataset.csv'
 data = []
-min_sup = 400
+min_sup = float(input('min_support:'))
+min_confi = float(input('min_confidence:'))
+start = time.time()
+
+
 
 try:
     f = open(path, 'r')
@@ -22,7 +30,7 @@ except:
     exit(1)
         
 
-
+#print(data)
 num_count1 = {}
 num_count2 = {}
 num_count3 = {}
@@ -95,11 +103,94 @@ for i in range(len(single_count2)):
                     num_count3[temp_str] = 0
                     temp_str = str(i)+","+str(j)+","+str(k)
                     number.append(temp_str)
-            
+                    
+for i in range(length):
+    n = int(data[i][0])
+    temp_str1 = ""
+    temp_str2 = ""
+    temp_str3 = ""
+    temp_str4 = ""
+    temp_str5 = ""
+    temp_str6 = ""
+    for j in range(1,n+1):
+        for k in range(j,n+1):
+            for m in range(k,n+1):
+                if ((data[i][j]!=data[i][k])and(data[i][j]!=data[i][m])and(data[i][m]!=data[i][k])):
+                    temp_str1 = data[i][j]+","+data[i][k]+","+data[i][m]
+                    temp_str2 = data[i][j]+","+data[i][m]+","+data[i][k]
+                    temp_str3 = data[i][k]+","+data[i][j]+","+data[i][m]
+                    temp_str4 = data[i][k]+","+data[i][m]+","+data[i][j]
+                    temp_str5 = data[i][m]+","+data[i][k]+","+data[i][j]
+                    temp_str6 = data[i][m]+","+data[i][j]+","+data[i][k]
+                    if(temp_str1 in num_count3):
+                        num_count3[temp_str1] +=1
+                    elif(temp_str2 in num_count3):
+                        num_count3[temp_str2] +=1
+                    elif(temp_str3 in num_count3):
+                        num_count3[temp_str3] +=1
+                    elif(temp_str4 in num_count3):
+                        num_count3[temp_str4] +=1
+                    elif(temp_str5 in num_count3):
+                        num_count3[temp_str5] +=1
+                    elif(temp_str6 in num_count3):
+                        num_count3[temp_str6] +=1
 
-print(min_sup)
-#print(num_count1)
-print(num_count2) 
-print(num_count3)  
-print(single_count2) 
-print(number)
+for key, value in dict(num_count3).items():
+    if value < min_sup:
+        del num_count3[key]                
+
+count3_list= []
+count3_value= []
+count2_list= []
+count2_value= []
+for key in (num_count3):
+    count3_list.append(key)
+    count3_value.append(num_count3[key])
+for key in (num_count2):
+    count2_list.append(key)
+    count2_value.append(num_count2[key])
+    
+for i in range(len(count3_list)):
+    count3_list[i]=count3_list[i].split(",")
+#print(count3_list)
+
+#print("minimum support :",min_sup)
+#print("minimum confidence :",min_confi,"\n")
+print("Numbers of data:",length)
+print("\n")
+with open('output.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    
+    for i in range(len(count2_list)):
+        count2_list[i]=count2_list[i].split(",")
+        for j in range(len(count3_list)):
+            if ((count2_list[i][0] in count3_list[j]) and (count2_list[i][1] in count3_list[j])):
+                temp3 = copy.deepcopy(count3_list[j])
+                temp3.remove(count2_list[i][0])
+                temp3.remove(count2_list[i][1])
+                confidence = int(count3_value[j])/int(count2_value[i])
+                if (confidence>min_confi):
+                    temp_str=count2_list[i][0]+","+count2_list[i][1]+"->"+temp3[0]
+                    print("Relation rules: {",count2_list[i][0],",",count2_list[i][1],"->",temp3[0],"}")
+                    print("Support:",(count3_value[j]/length))
+                    print("Confidence :",confidence)
+                    print("Count of",count3_list[j],':',count3_value[j])
+                    print("Count of",count2_list[i],":",count2_value[i])
+                    print("--------------------")
+                    writer.writerow([temp_str, count3_value[j]/length, confidence])
+
+
+#print(count3_list)
+#print(count3_value)
+#print(count2_list)
+#print(count2_value)
+#print(min_sup)
+#print(length)
+#print(num_count1) 
+#print(num_count2) 
+#print(num_count3)  
+#print(single_count2) 
+#print(number)
+end = time.time()
+
+print(end - start)
--- FP.py
+++ FP.py
@@ -1,8 +1,10 @@
 import csv
 
+
+
 path = 'dataset.csv'
 data = []
-min_sup = 500
+min_sup = float(input('min_support:'))
 
 try:
     f = open(path, 'r')
@@ -43,8 +45,8 @@ sort_list = []
 sort_data = []
 for key in sort_count1:
     sort_list.append(key)
-print(sort_list)
-print("-----------------")
+#print(sort_list)
+#print("-----------------")
 
 for i in range(length):
     n = int(data[i][0])
@@ -56,28 +58,148 @@ for i in range(length):
     sort_data.append(temp_str)
                 
                 
-print(sort_data)
-#####以下待研究
+#print(sort_data)
+
+# FP樹類
 class treeNode:
     def __init__(self, nameValue, numOccur, parentNode):
-        self.name = nameValue # 節點名稱
-        self.count = numOccur # 節點出現次數
-        self.nodeLink = None # 不同項集的相同項通過nodeLink連線在一起
-        self.parent = parentNode # 指向父節點
-        self.children = {} # 儲存葉子節點
+        self.name = nameValue  #節點元素名稱，在構造時初始化爲給定值
+        self.count = numOccur   # 出現次數，在構造時初始化爲給定值
+        self.nodeLink = None   # 指向下一個相似節點的指針，默認爲None
+        self.parent = parentNode   # 指向父節點的指針，在構造時初始化爲給定值
+        self.children = {}  # 指向子節點的字典，以子節點的元素名稱爲鍵，指向子節點的指針爲值，初始化爲空字典
 
+    # 增加節點的出現次數值
     def inc(self, numOccur):
-        """inc(對count變數增加給定值)
-        """
         self.count += numOccur
 
+    # 輸出節點和子節點的FP樹結構
     def disp(self, ind=1):
-        """disp(用於將樹以文字形式顯示)
-        """
-        print('  '*ind, self.name, ' ', self.count)
+        print(' ' * ind, self.name, ' ', self.count)
         for child in self.children.values():
-            child.disp(ind+1)
-            
-    def __lt__(self, other):
-        return self.count < other.count
+            child.disp(ind + 1)
+
+# =======================================================構建FP樹==================================================
+
+# 對不是第一個出現的節點，更新頭指針塊。就是添加到相似元素鏈表的尾部
+def updateHeader(nodeToTest, targetNode):
+    while (nodeToTest.nodeLink != None):
+        nodeToTest = nodeToTest.nodeLink
+    nodeToTest.nodeLink = targetNode
+
+# 根據一個排序過濾後的頻繁項更新FP樹
+def updateTree(items, inTree, headerTable, count):
+    if items[0] in inTree.children:
+        # 有該元素項時計數值+1
+        inTree.children[items[0]].inc(count)
+    else:
+        # 沒有這個元素項時創建一個新節點
+        inTree.children[items[0]] = treeNode(items[0], count, inTree)
+        # 更新頭指針表或前一個相似元素項節點的指針指向新節點
+        if headerTable[items[0]][1] == None:  # 如果是第一次出現，則在頭指針表中增加對該節點的指向
+            headerTable[items[0]][1] = inTree.children[items[0]]
+        else:
+            updateHeader(headerTable[items[0]][1], inTree.children[items[0]])
+
+    if len(items) > 1:
+        # 對剩下的元素項迭代調用updateTree函數
+        updateTree(items[1::], inTree.children[items[0]], headerTable, count)
+
+# 主程序。創建FP樹。dataSet爲事務集，爲一個字典，鍵爲每個事物，值爲該事物出現的次數。minSup爲最低支持度
+def createTree(dataSet, minSup=1):
+    # 第一次遍歷數據集，創建頭指針表
+    headerTable = {}
+    for trans in dataSet:
+        for item in trans:
+            headerTable[item] = headerTable.get(item, 0) + dataSet[trans]
+    # 移除不滿足最小支持度的元素項
+    keys = list(headerTable.keys()) # 因爲字典要求在迭代中不能修改，所以轉化爲列表
+    for k in keys:
+        if headerTable[k] < minSup:
+            del(headerTable[k])
+    # 空元素集，返回空
+    freqItemSet = set(headerTable.keys())
+    if len(freqItemSet) == 0:
+        return None, None
+    # 增加一個數據項，用於存放指向相似元素項指針
+    for k in headerTable:
+        headerTable[k] = [headerTable[k], None]  # 每個鍵的值，第一個爲個數，第二個爲下一個節點的位置
+    retTree = treeNode('Null Set', 1, None) # 根節點
+    # 第二次遍歷數據集，創建FP樹
+    for tranSet, count in dataSet.items():
+        localD = {} # 記錄頻繁1項集的全局頻率，用於排序
+        for item in tranSet:
+            if item in freqItemSet:   # 只考慮頻繁項
+                localD[item] = headerTable[item][0] # 注意這個[0]，因爲之前加過一個數據項
+        if len(localD) > 0:
+            orderedItems = [v[0] for v in sorted(localD.items(), key=lambda p: p[1], reverse=True)] # 排序
+            updateTree(orderedItems, retTree, headerTable, count) # 更新FP樹
+    return retTree, headerTable
+
+# =================================================查找元素條件模式基===============================================
+
+# 直接修改prefixPath的值，將當前節點leafNode添加到prefixPath的末尾，然後遞歸添加其父節點。
+# prefixPath就是一條從treeNode（包括treeNode）到根節點（不包括根節點）的路徑
+def ascendTree(leafNode, prefixPath):
+    if leafNode.parent != None:
+        prefixPath.append(leafNode.name)
+        ascendTree(leafNode.parent, prefixPath)
+
+# 爲給定元素項生成一個條件模式基（前綴路徑）。basePet表示輸入的頻繁項，treeNode爲當前FP樹中對應的第一個節點
+# 函數返回值即爲條件模式基condPats，用一個字典表示，鍵爲前綴路徑，值爲計數值。
+def findPrefixPath(basePat, treeNode):
+    condPats = {}  # 存儲條件模式基
+    while treeNode != None:
+        prefixPath = []  # 用於存儲前綴路徑
+        ascendTree(treeNode, prefixPath)  # 生成前綴路徑
+        if len(prefixPath) > 1:
+            condPats[frozenset(prefixPath[1:])] = treeNode.count  # 出現的數量就是當前葉子節點的數量
+        treeNode = treeNode.nodeLink  # 遍歷下一個相同元素
+    return condPats
+
+# =================================================遞歸查找頻繁項集===============================================
+# 根據事務集獲取FP樹和頻繁項。
+# 遍歷頻繁項，生成每個頻繁項的條件FP樹和條件FP樹的頻繁項
+# 這樣每個頻繁項與他條件FP樹的頻繁項都構成了頻繁項集
+
+# inTree和headerTable是由createTree()函數生成的事務集的FP樹。
+# minSup表示最小支持度。
+# preFix請傳入一個空集合（set([])），將在函數中用於保存當前前綴。
+# freqItemList請傳入一個空列表（[]），將用來儲存生成的頻繁項集。
+def mineTree(inTree, headerTable, minSup, preFix, freqItemList):
+    # 對頻繁項按出現的數量進行排序進行排序
+    sorted_headerTable = sorted(headerTable.items(), key=lambda p: p[1][0])  #返回重新排序的列表。每個元素是一個元組，[（key,[num,treeNode],()）
+    bigL = [v[0] for v in sorted_headerTable]  # 獲取頻繁項
+    for basePat in bigL:
+        newFreqSet = preFix.copy()  # 新的頻繁項集
+        newFreqSet.add(basePat)     # 當前前綴添加一個新元素
+        freqItemList.append(newFreqSet)  # 所有的頻繁項集列表
+        condPattBases = findPrefixPath(basePat, headerTable[basePat][1])  # 獲取條件模式基。就是basePat元素的所有前綴路徑。它像一個新的事務集
+        myCondTree, myHead = createTree(condPattBases, minSup)  # 創建條件FP樹
+
+        if myHead != None:
+            # 用於測試
+            print('conditional tree for:', newFreqSet)
+            myCondTree.disp()
+            mineTree(myCondTree, myHead, minSup, newFreqSet, freqItemList)  # 遞歸直到不再有元素
+
+
+# 將數據集轉化爲目標格式
+def createInitSet(dataSet):
+    retDict = {}
+    for trans in dataSet:
+        retDict[frozenset(trans)] = 1
+    return retDict
+
+if __name__=='__main__':
+    minSup =3
+    simpDat = sort_data  # 加載數據集
+    initSet = createInitSet(simpDat)  # 轉化爲符合格式的事務集
+    myFPtree, myHeaderTab = createTree(initSet, minSup)  # 形成FP樹
+    # myFPtree.disp()  # 打印樹
+
+    freqItems = []  # 用於存儲頻繁項集
+    mineTree(myFPtree, myHeaderTab, minSup, set([]), freqItems)  # 獲取頻繁項集
+    #print(freqItems)  # 打印頻繁項集
+    #print(sort_data)
     
